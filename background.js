c7d40476f7d76499dc87466181d2c63f0b0e3fe5
Wallin, Petter <petter.z.wallin@teliacompany.com>
2021-06-17 08:10:24 +0200

    dynamic date intervall

--- background.js
+++ background.js
@@ -1,63 +1,96 @@
+var skip = 0;
+const baseUrl = 'https://booking-api.mittvaccin.se/clinique/'
+const daysIntervall = 20; //Dagar framåt att söka
+
+//let clinique2 = {'name': 'Nacksta', 'cliniqueId': '1291', 'appointmentType': '7359'}; //restdos
+//let clinique3 = {'name': 'Timrå', 'cliniqueId': '1311', 'appointmentType': '10620'};
+//let clinique3 = {'name': 'Härnösand', 'cliniqueId': '1309', 'appointmentType': '12044'};
+//let clinique4 = {'name': 'kramfors', 'cliniqueId': '1323', 'appointmentType': '9132'};
+
+const cliniques = [
+    {'name': 'Nacksta', 'cliniqueId': '1291', 'appointmentType': '8853'},
+    {'name': 'Nacksta restdos', 'cliniqueId': '1291', 'appointmentType': '17418'}
+];
+
+
+
+const dateFrom = toFormattedDateString(new Date());
+const dateTo = toFormattedDateString(addDays(new Date(), daysIntervall));
+const period = dateFrom+'-'+dateTo;
+
+//cliniques.set('Nacksta', [baseUrl+'1291/appointments/8853/slots/210607-210613','https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210614-210620','https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210621-210627']);
+//cliniques.set('Timra', ['https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210607-210613', 'https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210614-210620','https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210621-210627']);
+//cliniques.set('Harnosand', ['https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210607-210613', 'https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210614-210620', 'https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210621-210627']);
+//cliniques.set('kramfors', ['https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210607-210613','https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210614-210620', 'https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210621-210627']);
+
+/*------------------------*/
+function toFormattedDateString(date){
+    return date.toISOString().substring(2, 10).replace(/[^0-9\.]+/g, "");
+}
+function addDays(date, days) {
+    const calculatedDate = date;
+    calculatedDate.setDate(date.getDate() + days);
+    return calculatedDate;
+}
 
 function parse(data,clinique) {
+    let foundTime = false;
     $(data).each(function (i, d) {
         $(d.slots).each(function (i, da) {
 
             if (da.available == true) {
                 console.log(new Date().toLocaleTimeString()+": Found a time at: "+ clinique.name);
-                //console.log(da.when);
+
                 console.log(d.date+' '+da.when)
                 let url = 'https://bokning.mittvaccin.se/klinik/'+clinique.cliniqueId+'/bokning';
                 console.log("Boka här: "+ url);
                 console.log("");
                 notification(url, clinique.name);
                 chrome.tabs.create({ url: url });
+                foundTime = true; //break loop
+
+                if(skip == 0){
+                    skip = 10;
+                }
+                return false;
             }
         });
+
+        if(foundTime) return false; //Found a time, abort loop
+
     });
 }
 
-let baseUrl = 'https://booking-api.mittvaccin.se/clinique/'
-let clinique1 = {'name': 'Nacksta', 'cliniqueId': '1291', 'appointmentType': '8853'};
-let clinique2 = {'name': 'Timrå', 'cliniqueId': '1311', 'appointmentType': '10620'};
-let clinique3 = {'name': 'Härnösand', 'cliniqueId': '1309', 'appointmentType': '12044'};
-let clinique4 = {'name': 'kramfors', 'cliniqueId': '1323', 'appointmentType': '9132'};
 
-let cliniques = [clinique1,clinique2,clinique3,clinique4];
-var periods = ['210607-210613', '210614-210620', '210621-210627']
-//cliniques.set('Nacksta', [baseUrl+'1291/appointments/8853/slots/210607-210613','https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210614-210620','https://booking-api.mittvaccin.se/clinique/1291/appointments/8853/slots/210621-210627']);
-//cliniques.set('Timra', ['https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210607-210613', 'https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210614-210620','https://booking-api.mittvaccin.se/clinique/1311/appointments/10620/slots/210621-210627']);
-//cliniques.set('Harnosand', ['https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210607-210613', 'https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210614-210620', 'https://booking-api.mittvaccin.se/clinique/1309/appointments/12044/slots/210621-210627']);
-//cliniques.set('kramfors', ['https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210607-210613','https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210614-210620', 'https://booking-api.mittvaccin.se/clinique/1323/appointments/9132/slots/210621-210627']);
 
 function execute() {
     cliniques.forEach(function (clinique, i) {
-        periods.forEach(function (p, i) {
-            let url = baseUrl+clinique.cliniqueId+'/appointments/'+clinique.appointmentType+'/slots/'+p;
-
+            let url = baseUrl+clinique.cliniqueId+'/appointments/'+clinique.appointmentType+'/slots/'+period;
             $.get(url, function (data, textStatus, jqXHR) {  // success callback
                 parse(data, clinique);
             });
         });
-    });
-
 }
 
 console.log("Running..");
 
 chrome.alarms.create("checkerAlarm", {
     delayInMinutes: 0,
-    periodInMinutes: 0.2
+    periodInMinutes: 0.1
 });
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
     console.log("alarm larm");
-   execute();
+    if(skip ==0 ){
+        execute();
+    } else{
+        console.log("skipping: " + skip)
+        skip--;
+    }
+
 });
 
 function notification(url, text) {
-
-
     var opt = {
         type: "basic",
         title: "Ny Vaccintid",
@@ -68,3 +101,5 @@ function notification(url, text) {
     chrome.notifications.create(url, opt);
 
 }
+
+
--- frontend.js
+++ frontend.js
@@ -1,5 +1,50 @@
+
 jQuery(document).ready(function () {
 
+    let path = window.location.pathname;
 
+
+    if(path.includes('1291')){
+        /*setTimeout(function() {
+
+            console.log('Nacksta');
+
+            var vue = document.querySelector('#app').__vue__
+            vue.$store.commit("setSelectedAppointmentType", {appType: 3184})
+            vue.$store.commit("setBookingTimer", {
+                bookingTimer: new Date().getTime()
+            });
+
+            vue.$router.push({name: "Booking2", params: {cliniqueId: 1291}});
+        },2000);
+        */
+
+        console.log('Nacksta');
+        setTimeout(function(){document.querySelectorAll('select')[0].selectedIndex = 2
+            document.querySelectorAll('select')[0].dispatchEvent(new Event('change'))
+
+        }
+        , 500);
+        setTimeout(function(){
+            document.querySelectorAll('select')[1].selectedIndex = 1
+            document.querySelectorAll('select')[1].dispatchEvent(new Event('change'))
+
+        },600)
+        setTimeout(function(){
+            document.head.innerHTML += '<script>alert = function(){}</script>';
+            window.alert=null;
+            document.querySelector('.save').dispatchEvent(new Event('click'));
+
+        },670);
+
+
+    }
 
 });
+
+function populate(){
+    document.querySelectorAll('select')[0].selectedIndex = 1
+    document.querySelectorAll('select')[0].dispatchEvent(new Event('change'))
+    document.querySelectorAll('select')[1].selectedIndex = 1
+    document.querySelectorAll('select')[1].dispatchEvent(new Event('change'))
+}
